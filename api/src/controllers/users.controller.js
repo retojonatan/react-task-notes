4388fcae1c8c6c00c7b8a9ab89b8c598d56635b4
Pyrex <retojonatan@gmail.com>
2021-07-06 00:07:06 -0300

    added user controllers

--- api/src/controllers/users.controller.js
+++ api/src/controllers/users.controller.js
@@ -1,23 +1,68 @@
+const User = require("../models/User");
+
 const usersCtrl = {};
 
-usersCtrl.getUsers = (req, res) => {
-  res.json({ message: "get" });
+usersCtrl.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-usersCtrl.getUser = (req, res) => {
-  res.json({ message: "get" });
+usersCtrl.getUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(204).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-usersCtrl.createUser = (req, res) => {
-  res.json("post");
+usersCtrl.createUser = async (req, res) => {
+  try {
+    const { username } = req.body;
+    const newUser = new User({ username });
+    await newUser.save();
+    res.json({ message: "User created" });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-usersCtrl.updateUser = (req, res) => {
-  res.json("put");
+usersCtrl.updateUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { username } = req.body;
+    const userUpdated = await User.findByIdAndUpdate(id, { username });
+    if (userUpdated) {
+      res.json({ message: "User updated" });
+    } else {
+      res.status(204).json({ message: "No user with parameters" });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-usersCtrl.deleteUser = (req, res) => {
-  res.json("delete");
+usersCtrl.deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userDeleted = await User.findByIdAndDelete(id);
+    if (userDeleted) {
+      res.json({ message: "User deleted" });
+    } else {
+      res.status(204).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = usersCtrl;
