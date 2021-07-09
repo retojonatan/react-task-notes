fff07d8d8a8f793160bd5bdfbde6a1c06fdad1d3
Pyrex <retojonatan@gmail.com>
2021-07-09 02:03:43 -0300

    create router dom and paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import "bootswatch/dist/materia/bootstrap.min.css";
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import NoteForm from "./components/NoteForm";
+import NotesList from "./components/NotesList";
+import UserForm from "./components/UserForm";
 
 const App = () => {
-  return <div>Hello Worldd</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path="/" component={NotesList} />
+          <Route path="/note/create/" component={NoteForm} />
+          <Route path="/note/edit/:id" component={NoteForm} />
+          <Route path="/user/create/" component={UserForm} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
