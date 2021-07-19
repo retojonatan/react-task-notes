f5694d460b02508aa5472d89b3d45d20a9d48ff2
Pyrex <retojonatan@gmail.com>
2021-07-19 02:44:53 -0300

    added states and handler for submit

--- frontend/src/components/NoteForm.js
+++ frontend/src/components/NoteForm.js
@@ -1,8 +1,20 @@
 import axios from "axios";
+import es from "date-fns/locale/es";
 import React, { useEffect, useState } from "react";
+import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+registerLocale("es", es);
+setDefaultLocale("es");
 
 const NoteForm = () => {
+  const initialState = {
+    userSelected: "",
+    title: "",
+    content: "",
+  };
   const [users, setUsers] = useState([]);
+  const [userNote, setUserNote] = useState(initialState);
+  const [date, setDate] = useState(new Date());
 
   const getUserData = async () => {
     const users = await axios.get("http://localhost:4000/api/users");
@@ -13,12 +25,22 @@ const NoteForm = () => {
     getUserData();
   }, []);
 
-  const handleSubmitNote = (e) => {
+  const handleSubmitNote = async (e) => {
     e.preventDefault();
+    const newNote = {
+      title: userNote.title,
+      content: userNote.content,
+      author: userNote.userSelected,
+      date,
+    };
+    await axios.post("http://localhost:4000/api/notes", newNote);
+  };
+  const handleDate = (date) => {
+    setDate(date);
   };
 
   const onSelectChange = (e) => {
-    console.log(e.target.value);
+    setUserNote({ ...userNote, [e.target.name]: e.target.value });
   };
 
   return (
@@ -45,7 +67,36 @@ const NoteForm = () => {
             </div>
 
             <div className="form-group">
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                name="title"
+                placeholder="title"
+                onChange={onSelectChange}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <textarea
+                type="text"
+                className="form-control"
+                name="content"
+                placeholder="content"
+                onChange={onSelectChange}
+                rows={2}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <DatePicker
+                className="mt-3 form-control"
+                selected={date}
+                onChange={handleDate}
+                locale="es"
+                required
+              />
             </div>
 
             <button className="btn btn-sm btn-dark mt-3" type="submit">
